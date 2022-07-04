4c6e48ea650e2807fb4d3220455be007ac7e8175
Berzan <berzan@tutanota.com>
2022-07-04 16:19:36 +0300

    add: Message account

--- programs/talkana/src/lib.rs
+++ programs/talkana/src/lib.rs
@@ -13,3 +13,17 @@ pub mod talkana {
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[account]
+pub struct Message {
+    pub author: Pubkey, // 32 bytes
+    pub timestamp: i64, // 8 bytes
+    pub topic: String, // 4 + 16*4 bytes
+    pub content: String, // 4 + 64*4 bytes
+}
+
+impl Message {
+    // Byte length that'll be rent on Solana
+    const LEN: usize = 32 + 8 + (4 + 16*4) + (4 + 64*4);
+    
+}
