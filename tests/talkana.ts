c9f13cef2bb09d6d6791c3fa254e457a6854e3a7
Berzan <berzan@tutanota.com>
2022-07-04 17:45:32 +0300

    add: new tests

--- tests/talkana.ts
+++ tests/talkana.ts
@@ -9,7 +9,7 @@ describe("talkana", () => {
 
   const program = anchor.workspace.Talkana as Program<Talkana>
 
-  it("Send a new tweet!", async () => {
+  it("Send a new message!", async () => {
     const msg = anchor.web3.Keypair.generate()
     
     const tx = await program.rpc.sendMessage("solana", "I love solana!", {
@@ -28,4 +28,41 @@ describe("talkana", () => {
     assert.equal(msgAccount.author.toBase58(), provider.wallet.publicKey.toBase58())
     assert.ok(msgAccount.timestamp)
   })
+
+  it("Send a message without topic!", async () => {
+    const msg = anchor.web3.Keypair.generate()
+
+    const tx = await program.rpc.sendMessage("", "Hey, everyone!", {
+      accounts: {
+        author: provider.wallet.publicKey,
+        message: msg.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [msg]
+    })
+
+    const msgAccount = await program.account.message.fetch(msg.publicKey)
+
+    assert.equal(msgAccount.topic, '')
+  })
+
+  it("Can't send a message whose content has more than 64 characters!", async () => {
+    try {
+      const msg = anchor.web3.Keypair.generate()
+      const tx = await program.rpc.sendMessage('test', 'I wanna send a long message'.repeat(10), {
+        accounts: {
+          author: provider.wallet.publicKey,
+          message: msg.publicKey,
+          systemProgram: anchor.web3.SystemProgram.programId,
+        },
+        signers: [msg]
+      })
+    } catch (e) {
+      assert.equal(e.error.errorMessage, "Content characters count shoudln't exceed 64!")
+      return
+    }
+    assert.fail("Not expected: Users can send messages whose content has more than 64 characters!")
+  })
+
+
 })
