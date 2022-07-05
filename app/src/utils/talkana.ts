dc99f4baf4108b472471cd34070fe9059fa05ee3
Berzan <berzan@tutanota.com>
2022-07-05 14:28:17 +0300

    add: TALKANA_IDL and TALKANA_PROGRAM_ID

--- app/src/utils/talkana.ts
+++ app/src/utils/talkana.ts
@@ -1,4 +1,9 @@
+import { Idl } from "@project-serum/anchor"
 import { Connection, PublicKey } from "@solana/web3.js"
+import idl from "../utils/idl.json"
+
+export const TALKANA_IDL: Idl = idl as Idl
+export const TALKANA_PROGRAM_ID: PublicKey = new PublicKey("6w18Ct9KbWY7qtVmkzZX8E8HLHhAT3qhHNCcjh5SuTT6")
 
 export const getAirdrop = async (publicKey: PublicKey, connection: Connection)  => {
     // Request 1 SOL airdrop
