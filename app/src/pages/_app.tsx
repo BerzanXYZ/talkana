a734808e1ffa32ebaac22219cd85d562a3461bc6
Berzan <berzan@tutanota.com>
2022-07-05 10:21:30 +0300

    add: solana related providers

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,9 +1,37 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import { useMemo } from 'react'
+import { clusterApiUrl } from '@solana/web3.js'
+import { GlowWalletAdapter, PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Can be set to 'devnet', 'testnet', or 'mainnet-beta'
+  const network = WalletAdapterNetwork.Devnet
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network])
+
+  // You have to configure the wallets you wanna use in your app
+  // otherwise unconfigured wallets won't work
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new GlowWalletAdapter(),
+      new SolflareWalletAdapter({network}),
+    ],
+    [network]
+  )
+
   return (
-      <Component {...pageProps} />
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
